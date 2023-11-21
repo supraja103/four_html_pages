71468d425c4ed5390dae27e727b10489084d76ce
supraja103 <suprajadesula103@gmail.com>
2023-11-21 12:02:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sup(request):
+    return render(request,'sup.html')
+def lav(request):
+    return render(request,'lav.html')
+def lakshmi(request):
+    return render(request,'lakshmi.html')
+def anand(request):
+    return render(request,'anand.html')
